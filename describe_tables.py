9639b5a3c478700dd5c5a8b0938149f60487342a
Marcelo Garcia <marcelomgarcia@gmail.com>
2025-11-02 11:14:49 +0000

    Initial commit

---
+++ describe_tables.py
@@ -0,0 +1,40 @@
+# Describe tables in a MySQL database
+
+import os
+import pymysql
+from dotenv import load_dotenv
+
+# Global variables for the database:
+db_host="localhost"
+db_user="root"
+db_protocol="tcp"
+db_port=3336
+
+def main():
+    load_dotenv()
+    db_root_passwd = os.environ["MYSQL_ROOT_PASSWORD"]
+
+    try:
+        conn = pymysql.connect(host=db_host,
+            user=db_user, 
+            password=db_root_passwd,
+            port=db_port)
+        if conn.open:
+            print("Connection OK")
+    except Exception as ee:
+        print("Error while connecting to database: ", ee)
+
+
+    cur = conn.cursor()
+
+    cur.execute("show databases")
+    print(cur.description)
+    print()
+
+    for row in cur:
+        print(row)
+    
+
+if __name__ == "__main__":
+    main()
+
