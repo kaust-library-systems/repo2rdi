174d292e8eb0725d6e28e93c4954756ff9f1c34c
Marcelo Garcia <marcelomgarcia@gmail.com>
2025-11-02 08:01:30 +0000

    UV init

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from repo2rdi!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ prod_ioi_describe_tables.sql
@@ -0,0 +1,18 @@
+USE prod_ioi;
+SELECT "Non_KAUST_Affiliated_Publications_for_FAAR" as '';
+DESCRIBE Non_KAUST_Affiliated_Publications_for_FAAR;
+SELECT "accessKeys" as '';
+DESCRIBE accessKeys;
+SELECT "Have a nice day" as '';
+
+emailTemplates                            
+groups                                   
+mappings                                
+messages                               
+orcids                                
+putCodes                             
+queryLog                            
+tokens                             
+userSelections                    
+users                            
++
