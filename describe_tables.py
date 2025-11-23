b2aa88e8f17f0d6b215365f4eecd5b3ce43dfbba
Marcelo Garcia <marcelomgarcia@gmail.com>
2025-11-23 11:09:27 +0000

    Basic functionality print tables from DB

--- describe_tables.py
+++ describe_tables.py
@@ -1,25 +1,45 @@
 # Describe tables in a MySQL database
 
 import os
+import sys
 import pymysql
+from pathlib import Path
+from typing import IO
 from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader
 
 # Global variables for the database:
 db_host="localhost"
 db_protocol="tcp"
 db_port=3336
 
-def describe_table(table: tuple) -> None:
+def describe_table(table: tuple) -> str:
     """Print table description in a nice way for humans"""
 
+    header = '''
+| Field | Type | Null | Key | Default | Extra |
+| ----- | ---- | ---- | --- | ------- | ----- |
+''' 
+
+    str_tmp = ""
     for row in table:
-        print("|".join(str(rr) for rr in row))
+        str_tmp += "| " + " | ".join(str(rr) for rr in row) + " |\n"
+
+    str_table = header + str_tmp
+
+    # print(str_table)
+
+    return str_table
         
 def main():
     load_dotenv()
     db_irts_user = os.environ["IRTS_USER"]
     db_irts_passwd = os.environ["IRTS_USER_PASSWD"]
-    db_name = os.environ["IRTS_DB"]
+    #db_name = os.environ["IRTS_DB"]
+    if len(sys.argv) != 2:
+        raise ValueError(f"Usage {sys.argv[0]} <db_name>")
+    else:
+        db_name = sys.argv[1]
     
     try:
         conn = pymysql.connect(host=db_host,
@@ -36,20 +56,25 @@ def main():
 
     cur.execute("show tables")
 
-    tables = [row[0] for row in cur]
-
-    # for table in tables:
-    #     cmd = f"describe {table}"
-    #     cur.execute(cmd)
-    #     table_desc = cur.fetchall()
-    #     print(type(table_desc))
-    #     describe_table(table_desc)
-
-    table = tables[0]
-    cmd = f"describe {table}"
-    cur.execute(cmd)
-    table_desc = cur.fetchall()
-    describe_table(table_desc)
+    db_tables_file = Path(f"{db_name}_tables.md")
+    print(db_tables_file)
+    with open(db_tables_file, "w") as ff:
+        tables = [row[0] for row in cur]
+        str_table = ""
+        print("Processing tables")
+        for table in tables:
+            if table == "groups":
+                # "Fix" tables with names that can conflict with
+                # system (MySQL) tables.
+                table = f"`{table}`"
+            print(f"Table: {table}")
+            ff.write((f"## {table}"))
+            cmd = f"describe {table}"
+            cur.execute(cmd)
+            table_desc = cur.fetchall()
+            str_table = describe_table(table_desc)
+            ff.write(str_table)
+            ff.write("")
 
 
     cur.close()
